030787d5bc97ba2ae62ba9a070af51d75f08c92b
gustavost26 <gteles@trivalor.pt>
2025-04-06 19:10:13 +0100

    First commit of the technology content platform

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,86 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { CardPost } from "@/components/CardPost";
+import logger from "@/logger";
 
-export default function Home() {
+import styles from './page.module.css'
+import Link from "next/link";
+import db from "../../prisma/db";
+
+
+// const post = {
+//   "id": 1,
+//   "cover": "https://raw.githubusercontent.com/viniciosneves/code-connect-assets/main/posts/introducao-ao-react.png",
+//   "title": "Introdução ao React",
+//   "slug": "introducao-ao-react",
+//   "body": "Neste post, vamos explorar os conceitos básicos do React, uma biblioteca JavaScript para construir interfaces de usuário. Vamos cobrir componentes, JSX e estados.",
+//   "markdown": "```javascript\nfunction HelloComponent() {\n  return <h1>Hello, world!</h1>;\n}\n```",
+//   "author": {
+//       "id": 101,
+//       "name": "Ana Beatriz",
+//       "username": "anabeatriz_dev",
+//       "avatar": "https://raw.githubusercontent.com/viniciosneves/code-connect-assets/main/authors/anabeatriz_dev.png"
+//   }
+// }
+
+async function getAllPosts(page, searchTerm) {
+  // const response = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=6`); 
+
+  // if(!response.ok) {
+  //   logger.error('Ops, alguma coisa correu mal!');
+  //   return [];
+  // }
+
+  // logger.info('Posts obtidos com sucesso!');
+  // return response.json();
+
+  const where = {};
+
+  if (searchTerm) {
+    where.title = {
+      contains: searchTerm,
+      mode: 'insensitive'
+    }
+  }
+
+  const perPage = 6;
+  const skip = (page - 1) * perPage;
+
+  const totalItems = await db.post.count({where});
+  const totalPages = Math.ceil(totalItems / perPage);
+
+  const prev = page > 1 ? page - 1 : null;
+  const next = page < totalPages ? page + 1 : null;
+
+  try {
+    const posts = await db.post.findMany({
+      take: perPage,
+      skip,
+      where,
+      orderBy: { createdAt: 'desc' },
+      include: {
+        author: true
+      }
+    });
+
+    return { data: posts, prev, next }; 
+  } catch (error) {
+    logger.error('Falha ao obter os posts!', { error });
+    return { data: [], prev: null, next: null }; 
+
+  }
+}
+
+export default async function Home({ searchParams }) {
+  const currentPage = parseInt(searchParams?.page || 1);
+  const searchTerm = searchParams?.q;
+  const { data: posts, prev, next } = await getAllPosts(currentPage, searchTerm);
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <main className={styles.grid}>
+      {posts.map(post => <CardPost key={post.id} post={post} />)}
+      <div className={styles.links}> 
+        {prev && <Link href={{pathname: '/', query: { page: prev, q: searchTerm }}}>Página Anterior</Link>}
+        {next && <Link href={{pathname: '/', query: { page: next, q: searchTerm }}}>Próxima Página</Link>}
+      </div>
+    </main>
   );
 }
---
+++ src/components/Aside/index.jsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+import styles from './aside.module.css';
+
+import logo from './logo.png';
+
+/**
+ * Componente Aside que renderiza o lado esquerdo da tela com o logo da Code Connect.
+ * @returns {JSX.Element} Um elemento <aside> contendo a logo.
+ */
+export const Aside = () => {
+  return (
+    <aside className={styles.aside}>
+      {/* Logo da Code Connect */}
+      {/* <img src="/logo.png" alt="Logo da Code Connect" /> */}
+      <Image src={logo} alt="Logo da Code Connect" />
+    </aside>
+  );
+}
